6e6e299ab8b156ade0674606f14f9541a12e1e05
Nick Hsieh <a1237255@gmail.com>
2018-06-05 18:34:36 +0800

    2018-06-05 update-1

--- laravel/resources/views/main/add.blade.php
+++ laravel/resources/views/main/add.blade.php
@@ -1,33 +1,60 @@
 @extends('layouts.master')
 @section('content')
-	<div style="margin:0px auto;">
-		<form class="form-horizontal" action="{{ route('main.add') }}" method="post">
+	<div class="container">
+		<form class="form-horizontal" id="add_data" action="{{ route('main.add') }}" method="post" onclick="return false;">
 			<input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
 			<div class="form-group">
 				<label class="col-sm-2 control-label">填寫人：</label>
 				<div class="col-sm-10">
-					<input class="form-control" id="form_name" name="form_name" type="text" value="">
+					<input class="form-control add_data_text" id="form_name" name="form_name" type="text" value="">
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="col-sm-2 control-label">預計完成時間：</label>
 				<div class="col-sm-10">
-					<input class="form-control" id="form_finish_time" name="form_finish_time" type="text" value="">
+					<input class="form-control add_data_text" id="form_finish_time" name="form_finish_time" type="text" value="">
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="col-sm-2 control-label">內容：</label>
 				<div class="col-sm-10">
-					<textarea class="form-control" rows="5" id="form_content" name="form_content"></textarea>
+					<textarea class="form-control add_data_text" rows="5" id="form_content" name="form_content"></textarea>
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="col-sm-2 control-label">備註：</label>
 				<div class="col-sm-10">
-					<input class="form-control" id="form_remark" name="form_remark" type="text" value="">
+					<input class="form-control add_data_text" id="form_remark" name="form_remark" type="text" value="">
 				</div>
 			</div>
-			<button type="submit" class="btn btn-default">Submit</button>
+			<div class="pull-right">
+				<button type="reset" class="btn btn-default">清除</button>
+				<button type="submit" id="click_submit" class="btn btn-default">送出</button>
+			</div>
 		</form>
 	</div>
+
+	<script type="text/javascript">
+		$('document').ready(function(){
+			$("#click_submit").click(function(){
+				var send_commit=true;
+				$(".add_data_text").each(function(){
+					console.log($(this).val());
+					var exp = $(this).val();
+					console.log(typeof(exp));
+					if(exp === null || exp==''){
+					    alert("is null");
+					    console.log('Y');
+					    send_commit=false;
+					}
+				});
+				
+				if(send_commit){
+					$("#add_data").submit();
+				}
+			});
+		});
+
+	</script>
+
 @endsection
